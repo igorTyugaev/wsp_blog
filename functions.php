a098dde935077290d5848b89444828dd5fb538cd
Igor Tyugaev <igor.tyugaev@gmail.com>
2021-06-15 21:46:49 +0500

    Add header

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
  * Set up theme defaults and registers support for various WordPress feaures.
  */
 add_action( 'after_setup_theme', function() {
-	load_theme_textdomain( 'bathe', get_theme_file_uri( 'languages' ) );
+	load_theme_textdomain( 'wsp', get_theme_file_uri( 'languages' ) );
 
 	add_theme_support( 'automatic-feed-links' );
 	add_theme_support( 'title-tag' );
@@ -22,7 +22,7 @@ add_action( 'after_setup_theme', function() {
 		'quote',
 		'link',
 	) );
-	add_theme_support( 'custom-background', apply_filters( 'bathe_custom_background_args', array(
+	add_theme_support( 'custom-background', apply_filters( 'wsp_custom_background_args', array(
 		'default-color' => 'ffffff',
 		'default-image' => '',
 	) ) );
@@ -43,7 +43,7 @@ add_action( 'after_setup_theme', function() {
 	) );
 
 	register_nav_menus( array(
-		'primary' => __( 'Primary Menu', 'bathe' ),
+		'primary' => __( 'Primary Menu', 'wsp' ),
 	) );
 } );
 
@@ -55,7 +55,7 @@ add_action( 'after_setup_theme', function() {
  * @global int $content_width
  */
 add_action( 'after_setup_theme', function() {
-	$GLOBALS['content_width'] = apply_filters( 'bathe_content_width', 960 );
+	$GLOBALS['content_width'] = apply_filters( 'wsp_content_width', 960 );
 }, 0 );
 
 /**
@@ -63,7 +63,7 @@ add_action( 'after_setup_theme', function() {
  */
 add_action( 'widgets_init', function() {
 	register_sidebar( array(
-		'name'          => __( 'Sidebar', 'bathe' ),
+		'name'          => __( 'Sidebar', 'wsp' ),
 		'id'            => 'sidebar-1',
 		'description'   => '',
 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
@@ -78,9 +78,9 @@ add_action( 'widgets_init', function() {
  */
 add_action( 'wp_enqueue_scripts', function() {
 
-	wp_enqueue_style( 'bathe-main', get_theme_file_uri( 'assets/css/main.css' ) );
+	wp_enqueue_style( 'wsp-main', get_theme_file_uri( 'assets/css/main.css' ) );
 
-	wp_enqueue_script( 'bathe-bundle', get_theme_file_uri( 'assets/js/bundle.js' ), array(), null, true );
+	wp_enqueue_script( 'wsp-bundle', get_theme_file_uri( 'assets/js/bundle.js' ), array(), null, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
--- header.php
+++ header.php
@@ -1,17 +1,68 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-	<head>
-		<meta charset="<?php bloginfo( 'charset' ); ?>" />
-		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<?php wp_head(); ?>
-	</head>
+<head>
+	<meta charset="<?php bloginfo('charset'); ?>"/>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<?php wp_head(); ?>
+</head>
 
-	<body <?php body_class(); ?>>
-		<?php wp_body_open(); ?>
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
 
-		<header class="header">
-			<h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
-		</header>
+<header class="header">
+	<div class="header__inner">
+		<div class="header__logo">
+			<a class="logo-header" href="<?php echo esc_url(home_url('/')); ?>">
+				<img class="logo-header__img"
+					 src="<?php echo get_template_directory_uri() ?>/assets/images/logos/logo_black.svg"
+					 alt="Logo: World Sci Publ">
+				<h3 class="logo-header__label subtitle subtitle_color_blue">
+					<?php bloginfo('name'); ?>
+				</h3>
+			</a>
+		</div>
 
-		<main id="primary" class="site-main" role="main">
+		<div class="header__bread-crumbs">
+			<ul class="bread-crumbs">
+				<div class="bread-crumbs__inner">
+					<li class="bread-crumbs__item">
+						<a class="text text_type_link text_color_gray-blue" href="#">
+							/ Все статьи
+						</a>
+					</li>
+					<li class="bread-crumbs__item">
+						<a class="text text_type_link text_color_gray-blue" href="#">
+							/ Сервис World Sci Publ
+						</a>
+					</li>
+					<li class="bread-crumbs__item">
+						<a class="text text_type_link text_color_gray-blue" href="#">
+							/ Почему давать гарантию...
+						</a>
+					</li>
+				</div>
+			</ul>
+		</div>
+
+		<div class="header__action">
+			<div class="action-header">
+				<div class="action-header__item">
+					<a class="text text_type_link text_color_black" href="tel:84951277926">+7
+						(495) 127 - 79 - 26
+					</a>
+				</div>
+
+				<div class="action-header__item">
+					<a class="text text_type_link text_color_black"
+					   href="<?php echo esc_url(home_url('/admin'));
+					   ?>">
+						Войти
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</header>
+
+<main id="primary" class="site-main" role="main">
