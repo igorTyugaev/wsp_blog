6a24ece046ec5bb91b70f600c142232fa4294be2
Igor Tyugaev <igor.tyugaev@gmail.com>
2021-07-23 19:32:16 +0500

    Fix state

--- template-parts/content/content-single.php
+++ template-parts/content/content-single.php
@@ -118,8 +118,147 @@
 					</div>
 				</div>
 				<div class="article__content">
-					<?php the_title('<h1 class="article__title title title_color_black">', '</h1>'); ?>
-					<?php the_content(); ?>
+					<?php
+					the_title('<h1 class="article__title title title_type_article">', '</h1>');
+					?>
+
+					<?php //the_content(); ?>
+
+					<div class="picture">
+						<img class="picture__img"
+							 src="https://images.unsplash.com/photo-1500622944204-b135684e99fd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2801&q=80"
+							 alt="article-img">
+						<span
+							class="picture__label text text_color_deep-gray text_align_center">
+							Рис. 1 - пример подписи под картинкой (опционально)
+						</span>
+					</div>
+
+					<h2 class="subtitle title_type_article">
+						Подзаголовок H2
+					</h2>
+
+					<p class="text text_type_article">
+						Очевидно, что давать гарантию публикации и индексации статьи, с
+						юридической точки зрения, не может ни одна компания, ведь данное
+						право принадлежит непосредственно изданию и международным базам.
+						Кроме того, ни один надёжный журнал не обещает публикацию до
+						успешного прохождения всех этапов рецензирования.
+					</p>
+
+					<ol class="numbered-list">
+						<li class="numbered-list__item">lore</li>
+						<li class="numbered-list__item">sdfdsf</li>
+						<li class="numbered-list__item">dsfsd</li>
+					</ol>
+
+					<p class="text text_type_article">
+						Мы же со своей стороны гарантируем Автору качество предоставляемых
+						услуг по доработке и сопровождению статьи, несём ответственность за
+						выполнение всех сопутствующих работ, то есть за основную
+						составляющую успешной публикации. Мы не платим взносы журналам, но
+						вкладываем в профессиональную доработку статьи 80% от общего
+						бюджета. World Sci Publ заинтересованы только в успешном результате,
+						так как прибыль рассчитывается именно от него.
+					</p>
+
+					<hr class="separator">
+
+					<p class="text text_type_article">
+						Мы уверены в качестве предлагаемых услуг, поэтому гарантируем
+						успешный результат. Но, конечно, также понимаем волнение Авторов,
+						сотрудничающих с нами впервые, поэтому прописываем в договоре все
+						риски и возможные пути решения форс-мажорных ситуаций.
+					</p>
+					<p class="text text_type_article">
+						Очевидно, что давать гарантию публикации и индексации статьи, с
+						юридической точки зрения, не может ни одна компания, ведь данное
+						право принадлежит непосредственно изданию и международным базам.
+						Кроме того, ни один надёжный журнал не обещает публикацию до
+						успешного прохождения всех этапов рецензирования.
+					</p>
+
+					<ul class="bulleted-list">
+						<li class="bulleted-list__item">lore</li>
+						<li class="bulleted-list__item">sdfdsf</li>
+						<li class="bulleted-list__item">dsfsd</li>
+					</ul>
+
+					<p class="text text_type_article">
+						Мы же со своей стороны гарантируем Автору качество предоставляемых
+						услуг по доработке и сопровождению статьи, несём ответственность за
+						выполнение всех сопутствующих работ, то есть за основную
+						составляющую успешной публикации. Мы не платим взносы журналам, но
+						вкладываем в профессиональную доработку статьи 80% от общего
+						бюджета. World Sci Publ заинтересованы только в успешном результате,
+						так как прибыль рассчитывается именно от него.
+					</p>
+					<p class="text text_type_article">
+						Мы уверены в качестве предлагаемых услуг, поэтому гарантируем
+						успешный результат. Но, конечно, также понимаем волнение Авторов,
+						сотрудничающих с нами впервые, поэтому прописываем в договоре все
+						риски и возможные пути решения форс-мажорных ситуаций.
+					</p>
+
+					<div class="quote">
+						<p class="quote__text tex text_color_deep-gray">
+							<span class="text_weight_bold">Цитата.</span> Очевидно,
+							что давать гарантию публикации и индексации
+							статьи, с юридической точки зрения, не может ни одна компания,
+							ведь данное право принадлежит непосредственно изданию и
+							международным базам. Кроме того, ни один надёжный журнал не
+							обещает публикацию до успешного прохождения всех этапов
+							рецензирования.
+						</p>
+					</div>
+
+					<p class="text text_type_article">
+						Очевидно, что давать гарантию публикации и индексации статьи, с
+						юридической точки зрения, не может ни одна компания, ведь данное
+						право принадлежит непосредственно изданию и международным базам.
+						Кроме того, ни один надёжный журнал не обещает публикацию до
+						успешного прохождения всех этапов рецензирования.
+					</p>
+
+					<div class="modal">
+						<div class="modal__title subtitle">
+							Укажите имя и почту
+							если хотите оставить
+							комментарий
+						</div>
+						<div class="modal__content">
+							<form class="auth-form" action="/">
+								<p class="auth-form__description">
+									Когда другой человек ответит на ваш комментарий – на
+									указанную почту придет письмо.
+								</p>
+								<div class="auth-form__inputs">
+									<input
+										class="auth-form__input input input_type_secondary"
+										name="email"
+										placeholder="Email"
+										type="email"
+										value=""
+										required/>
+									<input
+										class="auth-form__input input input_type_secondary"
+										name="name"
+										placeholder="Имя"
+										type="text"
+										value=""
+										required/>
+								</div>
+								<button class="auth-form__submit button button_type_main">
+									Оставить комментарий
+								</button>
+							</form>
+						</div>
+						<img class="modal__close"
+							 src="<?php echo
+							 get_template_directory_uri()
+							 ?>/assets/images/icons/close.svg"
+							 alt="close">
+					</div>
 				</div>
 				<div class="article__actions actions-article">
 					<div class="actions-article__actions">
@@ -191,16 +330,116 @@
 										 alt="user">
 								</div>
 							</div>
-							<input class="sender__input" id="message" name="message"
+							<input class="sender__input input input_type_primary"
+								   id="message"
+								   name="message"
 								   placeholder="Оставьте комментарий" type="text" value=""
 								   size="240" required/>
 							<button class="sender__submit button button_type_main">
 								Отправить
 							</button>
 						</div>
-
 						<div class="reviews__comments">
-
+							<div class="comments">
+								<div class="comments__title text text_weight_bold">
+									Комментарии:
+								</div>
+								<div class="comments__list">
+									<div class="comments__item">
+										<div class="message">
+											<div class="message__user">
+												<div class="user__avatar">
+													<img class="user__img"
+														 src="<?php echo
+														 get_template_directory_uri()
+														 ?>/assets/images/icons/user.svg"
+														 alt="user">
+												</div>
+												<span class="user__label text
+												text_color_deep-gray">
+													Ирина
+												</span>
+											</div>
+											<div class="message__content">
+												<div class="message__text text">
+													ведь данное право принадлежит
+													непосредственно изданию и международным
+													базам. Кроме того, ни один надёжный
+													журнал не обещает публикацию
+													до успешного прохождения всех этапов
+													рецензирования.
+												</div>
+												<div class="message__date text
+												text_color_deep-gray">
+													01.06.2021
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="comments__item">
+										<div class="message">
+											<div class="message__user">
+												<div class="user__avatar">
+													<img class="user__img"
+														 src="<?php echo
+														 get_template_directory_uri()
+														 ?>/assets/images/icons/user.svg"
+														 alt="user">
+												</div>
+												<span class="user__label text
+												text_color_deep-gray">
+													Ирина
+												</span>
+											</div>
+											<div class="message__content">
+												<div class="message__text text">
+													ведь данное право принадлежит
+													непосредственно изданию и международным
+													базам. Кроме того, ни один надёжный
+													журнал не обещает публикацию
+													до успешного прохождения всех этапов
+													рецензирования.
+												</div>
+												<div class="message__date text
+												text_color_deep-gray">
+													01.06.2021
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="comments__item">
+										<div class="message">
+											<div class="message__user">
+												<div class="user__avatar">
+													<img class="user__img"
+														 src="<?php echo
+														 get_template_directory_uri()
+														 ?>/assets/images/icons/user.svg"
+														 alt="user">
+												</div>
+												<span class="user__label text
+												text_color_deep-gray">
+													Ирина
+												</span>
+											</div>
+											<div class="message__content">
+												<div class="message__text text">
+													ведь данное право принадлежит
+													непосредственно изданию и международным
+													базам. Кроме того, ни один надёжный
+													журнал не обещает публикацию
+													до успешного прохождения всех этапов
+													рецензирования.
+												</div>
+												<div class="message__date text
+												text_color_deep-gray">
+													01.06.2021
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
